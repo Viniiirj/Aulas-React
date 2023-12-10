fca2db2636b0ecf32d6d64b35b5c72dc79fda443
viniiirj <vsr.rio@gmail.com>
2023-12-10 20:43:07 -0300

    criado primeiro componente

---
+++ meu-projeto/src/componentes/Meucomponente.js
@@ -0,0 +1,19 @@
+import styles from './Meucomponente.module.css'
+
+function Meucomponente(){
+    return(
+        <div className={styles.meucomponente}>
+            <h1>Titulo</h1>
+            <p className={styles.bla}>BLA BLA BLA
+            BLA BLA BLA
+            BLA BLA BLA <br/>
+            BLA BLA BLA
+            BLA BLA BLA
+            BLA BLA BLA
+            </p>
+            
+        </div>
+    )
+}
+
+export default Meucomponente
